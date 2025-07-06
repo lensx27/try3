79b50081bd54722365a328655567b914380e6c8b
Sugaritaz <rivera.zam24@gmail.com>
2025-07-06 18:49:38 -0500

    Se subió el proyecto

---
+++ src/ProyectoPoo/Cliente.java
@@ -0,0 +1,7 @@
+package ProyectoPoo;
+
+public class Cliente extends Persona {
+    public Cliente(String nombre, String correo, String telefono) {
+        super(nombre, correo, telefono);
+    }
+}
