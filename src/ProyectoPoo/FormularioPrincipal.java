2e05ba66d31957f50722b26a3ce596cc17856bee
lensx27 <losorganos1997@gmail.com>
2025-07-09 22:36:01 -0500

    Update FormularioPrincipal.java

--- src/ProyectoPoo/FormularioPrincipal.java
+++ src/ProyectoPoo/FormularioPrincipal.java
@@ -207,7 +207,7 @@ public class FormularioPrincipal extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FormularioPrincipal().setVisible(true);
+                 new LoginFrom().setVisible(true);
             }
         });
     }
